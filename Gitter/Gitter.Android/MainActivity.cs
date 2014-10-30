1d7cb73f4a472325e6ef850b4ec46e35fc10051b
Dennis Daume <daume.dennis@gmail.com>
2014-10-30 19:03:35 +0100

    Created the Android and PCL projects

---
+++ Gitter/Gitter.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace Gitter.Droid
+{
+    [Activity(Label = "Gitter", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+            SetPage(App.GetMainPage());
+        }
+    }
+}
