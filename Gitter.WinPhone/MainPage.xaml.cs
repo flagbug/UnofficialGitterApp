e0f8f0d1633f2c6c5a7ad9050c4584771bb87396
Dennis Daume <daume.dennis@gmail.com>
2014-11-05 21:21:52 +0100

    Added a Windows Phone target project

---
+++ Gitter.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Phone.Controls;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace Gitter.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+            var bootstrapper = RxApp.SuspensionHost.GetAppState<AppBootstrapper>();
+            bootstrapper.CreateMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
