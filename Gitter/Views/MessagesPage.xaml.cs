d7cfc5b71414005d26ac8af2609cc2eaa050a29e
Jan Karger <punkerat76@gmail.com>
2014-11-16 22:23:02 +0100

    f... xamarin tabs

--- Gitter/Views/MessagesPage.xaml.cs
+++ Gitter/Views/MessagesPage.xaml.cs
@@ -6,7 +6,7 @@ using Xamarin.Forms;
 
 namespace Gitter.Views
 {
-	public partial class MessagesPage : MasterDetailPage, IViewFor<MessagesViewModel>
+    public partial class MessagesPage : MasterDetailPage, IViewFor<MessagesViewModel>
     {
         public static readonly BindableProperty ViewModelProperty =
             BindableProperty.Create<MessagesPage, MessagesViewModel>(x => x.ViewModel, default(MessagesViewModel));
